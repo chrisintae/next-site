af397889428ccc48e9802fd5064ada56edc289b6
chrisintae <hello@chrisintae.com>
2021-07-27 23:00:52 -0400

    Update index and global.scss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,25 @@
+import Hero from '@comps/Hero'
 import Layout from '@comps/layout'
+import TwoColumn from '@comps/TwoColumn'
 
 export default function Home() {
   return (
     <Layout
       globalClass='app'
+      companyName='HomeBuyHome'
       siteTitle='HomeBuyHome App | Download now in the App Store'
       siteDescription=' Find your next home by comparing what is most important to you with ease.'
       mainCtaLabel='How it works'
       mainCtaHref='#'
-      facebookImgSrc=''
+      facebookImgSrc='/Icon_Facebook.svg'
       facebookImgAlt='Facebook'
-      instagramImgSrc=''
+      instagramImgSrc='/Icon_Instagram.svg'
       instagramImgAlt='Instagram'
-      twitterImgSrc=''
+      twitterImgSrc='/Icon_Twitter.svg'
       twitterImgAlt='Twitter'
-      linkedInImgSrc=''
+      linkedInImgSrc='/Icon_LinkedIn.svg'
       linkedInImgAlt='LinkedIn'
-      youTubeImgSrc=''
+      youTubeImgSrc='/Icon_YouTube.svg'
       youTubeImgAlt='YouTube'
       footerCtaSub='HomeBuyHome App'
       footerCtaHeadline='Download today'
@@ -24,6 +27,38 @@ export default function Home() {
       appStoreImgSrc='/Badge_AppStore.svg'
       appStoreImgAlt='App Store Badge'
     >
+      <Hero
+        headline='Discover what makes a home. Not just a house.'
+        subcopy='Easily distinguish the amenities that matter most with a side-by-side comparison guide before you purchase your next home.'
+        appStoreUrl='#'
+        appStoreImgSrc='/Badge_AppStore.svg'
+        appStoreImgAlt='App Store Badge'
+      />
+
+      <TwoColumn
+        subhead='Step 1'
+        headline='Enter addresses of your favorite listings'
+        subcopy='Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      />
+
+      <TwoColumn
+        subhead='Step 2'
+        headline='Compare your most critical home attributes'
+        subcopy='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+      />
+
+      <TwoColumn
+        subhead='Step 3'
+        headline='Find the home that matches your needs and wants'
+        subcopy='Make informed decisions, quickly, so you can land your dream home.'
+      />
+
+      <TwoColumn
+        themeClass='theme-black'
+        subhead='Latest Releases'
+        headline='Your experience is our passion'
+        subcopy='Stay up-to-date with new functionality or updates to designs to give you the advantage in this swiftly moving housing market.'
+      />
 
     </Layout>
   )
